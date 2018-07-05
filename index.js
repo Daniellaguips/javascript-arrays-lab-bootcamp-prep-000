f5c0f4f6b0fde73f6d3668d2654668c01482ff89
daniellaguips@gmail.com Guimaraes <daniellaguips@gmail.com>
2018-07-05 20:42:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Add your functions and code here
+function destructivelyAppendKitten(name){
+ kittens.push(name)
+  return kittens
+}
+
+function destructivelyPrependKitten(name){
+  kittens.unshift(name)
+  return kittens
+}
+
+function destructivelyRemoveLastKitten(){
+  kittens.pop()
+  return kittens
+}
+
+function destructivelyRemoveFirstKitten(){
+  kittens.shift()
+  return kittens
+}
+
+function appendKitten(name){
+  return kittens.appendKitten()
+}
+
+function prependKitten(name){
+  return [kitten, ...array]
+}
+
+function removeLastKitten(){
+  return slice(0, kitten.length-1)
+}
